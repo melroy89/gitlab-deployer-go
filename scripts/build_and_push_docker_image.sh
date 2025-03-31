f07a38067d14892c76b416fe482135a435aa61a6
Melroy van den Berg <melroy@melroy.org>
2025-03-31 15:49:58 +0200

    Push new docker images

--- scripts/build_and_push_docker_image.sh
+++ scripts/build_and_push_docker_image.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-docker build -t danger89/gitlab-deployer-go .
+docker build -t danger89/gitlab-deployer-go:latest -t registry.melroy.org/melroy/gitlab-artifact-deployer-go/artifact-deployer:latest .
+
+# Publish to both GitLab Registry and Docker Hub
 docker push danger89/gitlab-deployer-go:latest
+docker push registry.melroy.org/melroy/gitlab-artifact-deployer-go/artifact-deployer:latest
